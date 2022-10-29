d306842b8e03030feb6fbdb3dd7c6ce17eb7f1c2
Sadık Efe Kartav <115922435+iamalreadynoob@users.noreply.github.com>
2022-10-29 11:01:59 +0300

    Update Gact.java

--- expandedBoolean/Gact.java
+++ expandedBoolean/Gact.java
@@ -4,7 +4,8 @@ public class Gact
 {
 	
 	private double truth;
-	private Boolean neutralize = null;
+	private TruthLevels neutralBehave = TruthLevels.NEUT;
+	private Boolean tiresiasBehaviour = null;
 		
 	public Gact(double truth)
 	{
@@ -19,19 +20,23 @@ public class Gact
 	public void setterTruth(double truth) throws InvalidPointException
 	{
 
-		if(truth < 1 && truth > 0) this.truth = truth;
+		if(truth <= 1 && truth >= 0) this.truth = truth;
 		else throw new InvalidPointException("Truth value which is defined is out of the limits");
 		
 	}
 	
-	public boolean getterNeutralize()
+	public boolean getterTiresiasBehaviour(Reference point) throws InvalidPointException
 	{
-		return neutralize;
+		
+		if(level(point) == TruthLevels.TIRESIAS) return tiresiasBehaviour;
+		else throw new InvalidPointException("This is not TIRESIAS");
+		
 	}
 	
-	public void setterNeutralize(boolean newBehave)
+	public void setterTiresiasBehaviour(boolean tiresiasBehaviour, Reference point) throws InvalidPointException
 	{
-		neutralize = newBehave;
+		if(level(point) == TruthLevels.TIRESIAS) this.tiresiasBehaviour = tiresiasBehaviour;
+		else throw new InvalidPointException("This is not TIRESIAS");
 	}
 	
 	
@@ -40,15 +45,7 @@ public class Gact
 		
 		if(truth == 0) return TruthLevels.FALSE;	
 		else if(truth < point.getterReference()) return TruthLevels.AFALSE;
-		else if(truth == point.getterReference()) 
-		{
-			
-			ExpandedNeutral newNeutral = new ExpandedNeutral();
-			neutralize = newNeutral.setDefaultBehave();
-			
-			return TruthLevels.NEUTRAL;
-			
-		}
+		else if(truth == point.getterReference()) return neutralBehave;
 		else if(truth > point.getterReference() && truth != 1) return TruthLevels.ATRUE;
 		else return TruthLevels.TRUE;
 		
@@ -56,6 +53,9 @@ public class Gact
 	
 	
 	
+	public void changeNeutralBehaviour(Reference point, TruthLevels newNeutral) throws InvalidPointException
+	{
+		ExpandedNeutral.setBahave(neutralBehave, point, newNeutral);
+	}
 	
-
 }
